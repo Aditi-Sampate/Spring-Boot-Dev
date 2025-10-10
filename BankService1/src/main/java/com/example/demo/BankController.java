bdf56686d59d6fb64d89fdfe576f9a39271f40f3
unknown <aditisampate@gmail.com>
2025-10-11 00:32:35 +0530

    11/oct/2025 12:30 AM - first Spring Boot Application

---
+++ BankService1/src/main/java/com/example/demo/BankController.java
@@ -0,0 +1,65 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BankController {
+
+	@PostMapping
+	String addCustomer(@RequestBody Customer c) {
+		
+		if(c.getAge()<18) 
+		{
+			return c.getName()+ "You are not eligible";
+		}
+		return c.getName()+"Your account is opened";
+		
+	}
+	
+}
+
+
+
+class Customer{
+
+	private String name;
+	private String address;
+	private int age;
+	private long mobileno;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	public long getMobileno() {
+		return mobileno;
+	}
+	public void setMobileno(long mobileno) {
+		this.mobileno = mobileno;
+	}
+	
+	
+}
