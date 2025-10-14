e1e1e2ac07cec3a3358442a1ad5249fe28147ef2
unknown <aditisampate@gmail.com>
2025-10-14 22:58:03 +0530

    14/oct/2025 10:56 PM - Implemented Bank Service Application in Spring Boot

---
+++ BankService4/src/main/java/com/example/demo/BankController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BankController {
+
+	@PostMapping("addCustomer")
+	public String addCustomer(@RequestBody Customer c)
+	{
+		if(c.getAge() > 18)
+		{
+			return "Your Account is Created";
+		}
+		
+		return "Your account is not Created.";
+	}
+	
+}
---
+++ BankService4/src/main/java/com/example/demo/Customer.java
@@ -0,0 +1,60 @@
+package com.example.demo;
+
+public class Customer {
+
+	
+	String CustomerName;
+	String address;
+	int age;
+	String BankName;
+	String location;
+	
+	
+	public String getCustomerName()
+	{
+		return CustomerName;
+	}
+	public void setCustomerName(String CustomerName)
+	{
+		this.CustomerName = CustomerName;
+	}
+	
+	
+	public String getAddress()
+	{
+		return address;
+	}
+	public void setAddress(String address)
+	{
+		this.address = address;
+	}
+	
+	
+	public int getAge()
+	{
+		return age;
+	}
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+	
+	
+	public String getBankName() {
+		return BankName;
+	}
+	public void setBankName(String bankName) {
+		BankName = bankName;
+	}
+	
+	
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+	
+	
+}
