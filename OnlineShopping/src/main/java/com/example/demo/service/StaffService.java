8e3dafb4389d595ca8fbb07fd7341c550b531c97
unknown <aditisampate@gmail.com>
2025-10-16 00:44:57 +0530

    16/oct/2025 - 12:43 AM - Implementing the Postmapping & Getmapping to featch Data from database in Spring Boot

---
+++ OnlineShopping/src/main/java/com/example/demo/service/StaffService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Staff;
+
+public class StaffService {
+
+	public String saveStaff(Staff staff) 
+	{
+		return "Staff added in database";
+	
+	}
+	
+}
